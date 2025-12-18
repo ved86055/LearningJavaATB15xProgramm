e182485ca9f90639ada21ddcdfd29c42e395f4a4
Ved86055 <vedvshinde11@gmail.com>
2025-12-18 22:03:32 +0530

    Datatype

---
+++ src/datatype03tablenine.java
@@ -0,0 +1,22 @@
+public class datatype03tablenine
+{
+   public static void main(String[] args)
+   {
+       int i =9;
+       int r1=i;
+       int r2=i*2;
+       int r3=i*3;
+       int r4=i*4;
+       int r5=i*5;
+       int r6=i*6;
+       int r7=i*7;
+       int r8=i*8;
+       int r9=i*9;
+       int r10=i*10;
+
+
+       System.out.printf
+               ("r1=%d%n r2=%d%n r3=%d%n r4=%d%n r5=%d%n r6=%d%n r7=%d%n r8=%d%n r9=%d%n r10=%d%n",r1,r2,r3,r4,r5,r6,r7,r8,r9,r10);
+
+    }
+}
