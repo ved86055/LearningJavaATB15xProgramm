369393ef4b3a55bda8766120e1ac8ea391d40e25
Ved86055 <vedvshinde11@gmail.com>
2025-12-18 21:39:18 +0530

    Datatype

---
+++ src/datatype01.java
@@ -0,0 +1,17 @@
+public class datatype01
+{
+    public static void main(String []args)
+    {
+        String email="Suraj@gmail,com";
+        int age=12;
+        double d= 12.9;
+        float f=12.4f;
+        long mobile =1234566789l;
+
+        System.out.println(email);
+        System.out.println(age);
+        System.out.println(d);
+        System.out.println(f);
+        System.out.println(mobile);
+    }
+}
