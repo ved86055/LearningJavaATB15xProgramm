d883b65aba924f39f5b808e1001e226cd46b0b5f
Ved86055 <vedvshinde11@gmail.com>
2025-12-18 21:48:17 +0530

    Datatype

---
+++ src/datatype02.java
@@ -0,0 +1,11 @@
+public class datatype02
+{
+    public static void main(String []args)
+    {
+        int age=12;
+        String name="suraj";
+        float mark=65.5f;
+
+        System.out.printf("age=%d, name=%s ,mark=%.1f%n",age,name,mark);
+    }
+}
