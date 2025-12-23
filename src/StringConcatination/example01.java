9c456dbfc350bf021cd3d75124c620b97fc05358
Ved86055 <vedvshinde11@gmail.com>
2025-12-23 16:35:57 +0530

    ConcatString

---
+++ src/StringConcatination/example01.java
@@ -0,0 +1,21 @@
+package StringConcatination;
+
+public class example01
+{
+    public static void main(String[]args)
+    {
+        int a =200;
+        int b =25;
+
+        String c = "heyved";
+        String d="gmved ";
+
+        System.out.println(a+b+c+d);            //225hey vedgm ved
+        System.out.println(c+d+a+b) ;                            //heyvedgmved 20025
+
+        System.out.println(c+d+a+b+c+d);   //heyvedgmved 20025heyvedgmved
+
+        System.out.println(c+d+(a+b));
+
+    }
+}
