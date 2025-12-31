23f2afb418a227a599702c415818ac5520cffb58
Ved86055 <vedvshinde11@gmail.com>
2025-12-31 11:38:39 +0530

    IncrementDecrement

---
+++ src/IncrementDecrement_operator/Ex01.java
@@ -0,0 +1,45 @@
+package IncrementDecrement_operator;
+
+public class Ex01
+{
+    public static void main(String[]args)
+    {
+        /* Ex01
+        int a =6;
+        int b = ++a;
+        System.out.println(b);      //7
+        System.out.println(a);       //7
+
+       Ex02.
+        int a=44;
+        int b =a++;
+        System.out.println(a);  //45
+        System.out.println(b);//44
+
+
+
+        int e=34;
+       // System.out.println(++e + e);   //70
+        System.out.println(e++ + e);  //69
+        System.out.println(e);
+
+
+
+        int p =10;
+        System.out.println(p++ + ++p);   //10 +12 =22
+        System.out.println(p);       //12
+        
+
+
+        int a =50;
+        System.out.println( a++ + a++ + a++);  //50+51+52
+        System.out.println(a);   //53
+
+         */
+
+        int c = 3;
+        int b = c++ * ++c;    //3*5
+        System.out.println(b);   //valur of c 5
+
+    }
+}
