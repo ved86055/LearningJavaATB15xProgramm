dae28289b6485a324f177193e5f25fe062578edb
Ved86055 <vedvshinde11@gmail.com>
2026-01-20 08:58:05 +0530

    Arrayssum

---
+++ src/Arrays/sum_of_Arrys.java
@@ -0,0 +1,21 @@
+package Arrays;
+
+public class sum_of_Arrys
+{
+    public static void main(String[] args)
+    {
+        int marks[]={12,12,10,10};
+        int sum=0;
+
+        for (int i=0;i<marks.length;i++)
+        {
+            sum=sum+marks[i];
+        }
+
+        System.out.println(sum);
+
+
+
+
+    }
+}
