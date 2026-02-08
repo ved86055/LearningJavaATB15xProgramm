68a917f72a8f30407f8d90d7abf9e19b01ad326c
Ved86055 <vedvshinde11@gmail.com>
2026-02-08 19:15:08 +0530

    Exception Handling

---
+++ src/Exception_Handling/checked_exception.java
@@ -0,0 +1,30 @@
+package Exception_Handling;
+
+public class checked_exception
+{
+    public static void main(String[] args) throws InterruptedException {
+
+        int age=12;
+        System.out.println(age);
+
+        System.out.println("there is also one time ");
+
+        System.out.println("there is also as well as ");
+
+
+         Thread.sleep(1000);
+
+        System.out.println("oved prakash shinde");
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
